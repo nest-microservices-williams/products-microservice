f5f7b622240dd0019edabe3fc41e125537321b5c
Williams Mata <williams.rm99@gmail.com>
2024-08-30 12:23:21 -0400

    Add env

---
+++ src/config/envs.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  // DATABASE_URL: string;
+}
+
+const envVarsSchema = joi.object<EnvVars>({
+  PORT: joi.number().default(3000),
+  // DATABASE_URL: joi.string().required(),
+});
+
+function validateEnv<T>(
+  schema: joi.ObjectSchema<T>,
+  env: NodeJS.ProcessEnv = process.env,
+): T {
+  const { value, error } = schema.validate(env, {
+    allowUnknown: true,
+    convert: true,
+  });
+
+  if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+  }
+
+  return value;
+}
+
+type LowerCaseKeys<T> = {
+  [K in keyof T as Lowercase<string & K>]: T[K];
+};
+
+const validatedEnv = validateEnv(envVarsSchema);
+
+export const envs: LowerCaseKeys<EnvVars> = {
+  port: validatedEnv.PORT,
+  // database_url: validatedEnv.DATABASE_URL,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(envs.port);
 }
 bootstrap();
