bb6acbfb21741cd6e1d5292fb164ba0dddb372bc
williamsmata <williams.rm99@gmail.com>
2024-09-17 15:05:49 -0400

    Change TCP to NATS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,21 +4,29 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const envVarsSchema = joi.object<EnvVars>({
   PORT: joi.number().default(3000),
   DATABASE_URL: joi.string().required(),
+  NATS_SERVERS: joi.array().items(joi.string()).required(),
 });
 
 function validateEnv<T>(
   schema: joi.ObjectSchema<T>,
   env: NodeJS.ProcessEnv = process.env,
 ): T {
-  const { value, error } = schema.validate(env, {
-    allowUnknown: true,
-    convert: true,
-  });
+  const { value, error } = schema.validate(
+    {
+      ...env,
+      NATS_SERVERS: env.NATS_SERVERS?.split(','),
+    },
+    {
+      allowUnknown: true,
+      convert: true,
+    },
+  );
 
   if (error) {
     throw new Error(`Config validation error: ${error.message}`);
@@ -36,4 +44,5 @@ const validatedEnv = validateEnv(envVarsSchema);
 export const envs: LowerCaseKeys<EnvVars> = {
   port: validatedEnv.PORT,
   database_url: validatedEnv.DATABASE_URL,
+  nats_servers: validatedEnv.NATS_SERVERS,
 };
